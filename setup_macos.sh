55bf6f8673504946163037ef0368fe4422fab5d3
ryo-11 <63440731+ryosuke-11@users.noreply.github.com>
2022-01-02 19:15:19 +0900

    add maxos setting

---
+++ setup_macos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# *Findler関連
+defaults write com.apple.finder ShowPathbar -bool true # パスを表示
+defaults write com.apple.finder ShowStatusBar -bool true    # ステータスバーを表示
+defaults write com.apple.finder ShowTabView -bool true    # タブバーを表示
+
+# *トラックパッド
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true # 三本指ドラッグ
+
+# *スクリーンショット関連
+mkdir -p "$HOME/Screenshots" # フォルダを作成
+defaults write com.apple.screencapture location "$HOME/Screenshots" # 作成したフォルダに保存
+defaults write com.apple.screencapture type jpg # デフォルトのpngからjpgにする
+
+# 再起動
+killall Finder
+killall Dock
--- setup_screenshots.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p "$HOME/Screenshots"
-defaults write com.apple.screencapture location "$HOME/Screenshots"
-defaults write com.apple.screencapture type jpg
