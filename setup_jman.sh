c83d37ff7acf61d3b97002fc1fbbb8c71b1f9e53
ryo-11 <63440731+ryosuke-11@users.noreply.github.com>
2022-01-04 16:09:51 +0900

    add setup jman script

---
+++ setup_jman.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting jman Setup >>>\n"
+
+# コメントアウト
+sudo sed -i -e "s/^JNROFF[( ))(	)][( ))(	)]*\/usr\/bin\/groff -Tnippon -mandocj -c$/# &/" /etc/man.conf
+sudo sed -i -e "s/^PAGER[( ))(	)][( ))(	)]*\/usr\/bin\/less -is$/# &/" /etc/man.conf
+sudo sed -i -e "s/^BROWSER[( ))(	)][( ))(	)]*\/usr\/bin\/less -is$/# &/" /etc/man.conf
+
+# 書き換え
+if grep -E "^JNROFF /usr\/bin\/groff -Dutf8 -Tutf8 -mandoc -mja -E$" /etc/man.conf >/dev/null; then
+  echo New JNROFF exist.
+else
+  sudo sed -i -e "s/JNROFF[( ))(	)][( ))(	)]*\/usr\/bin\/groff -Tnippon -mandocj -c$/&\nJNROFF \/usr\/bin\/groff -Dutf8 -Tutf8 -mandoc -mja -E/" /etc/man.conf
+fi
+
+if grep -E "^PAGER /usr/bin/less -isr$" /etc/man.conf >/dev/null; then
+  echo New PAGER exist.
+else
+  sudo sed -i -e "s/# PAGER[( ))(	)][( ))(	)]*\/usr\/bin\/less -is$/&\nPAGER \/usr\/bin\/less -isr/" /etc/man.conf
+fi
+
+if grep -E "^BROWSER /usr/bin/less -isr$" /etc/man.conf >/dev/null; then
+  echo New BROWSER exist.
+else
+  sudo sed -i -e "s/# BROWSER[( ))(	)][( ))(	)]*\/usr\/bin\/less -is$/&\nBROWSER \/usr\/bin\/less -isr/" /etc/man.conf
+fi
+
+# 日本語訳のインストール
+cd ~/Downloads
+curl -O http://linuxjm.osdn.jp/man-pages-ja-20211215.tar.gz
+tar xfz man-pages-ja-20211215.tar.gz
+cd man-pages-ja-20211215
+make config
+make install
+
+# install folder: /opt/homebrew/share/man/
+# uname of page owner [root] ?: mac username
+# group of page owner [root] ?: admin
