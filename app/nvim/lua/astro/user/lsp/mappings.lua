30aba03d184727c59abef740cef9fedab2eb9b95
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-01-02 10:41:19 +0900

    update key mapping desc for nvim

--- app/nvim/lua/astro/user/lsp/mappings.lua
+++ app/nvim/lua/astro/user/lsp/mappings.lua
@@ -1,29 +1,29 @@
 return {
   n = {
     -- lspsaga
-    -- 重複la ["<leader>ca"] = { "<cmd>:Lspsaga code_action<cr>", desc = "利用可能なコードアクションを見る" },
-    -- 重複lr ["<leader>rn"] = { "<cmd>:Lspsaga rename<cr>", desc = "リネームする" },
-    -- 重複[d ["[d"] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "バッファ内の前の診断にジャンプ" },
-    -- 重複]d ["]d"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "バッファ内の次の診断にジャンプ" },
-    -- 重複K ["K"] = { "<cmd>:Lspsaga hover_doc<cr>", desc = "カーソルの下にあるもののドキュメントを表示" },
-    -- 重複lS ["<leader>o"] = { "<cmd>:Lspsaga outline<cr>", desc = "右側にアウトラインを表示" },
-    -- 重複ld ["<leader>d"] = { "<cmd>Lspsaga show_cursor_diagnostics<cr>", desc = "カーソルに関する診断を表示" },
-    ["<leader>ld"] = { "<cmd>Lspsaga show_line_diagnostics<cr>", desc = "行の診断を表示する" }, -- <leader>Dから変更し、行の診断を上書き（場所も表示されるため）
-    ["<leader>lgf"] = { "<cmd>Lspsaga lsp_finder<CR>", desc = "定義とリファレンスの表示" }, -- gfから変更
-    ["<leader>lgD"] = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "宣言まで取得" }, -- gDから変更
+    -- 重複la ["<leader>ca"] = { "<cmd>:Lspsaga code_action<cr>" }  -- 利用可能なコードアクションを見る
+    -- 重複lr ["<leader>rn"] = { "<cmd>:Lspsaga rename<cr>" }  -- リネームする
+    -- 重複[d ["[d"] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>" }  -- バッファ内の前の診断にジャンプ
+    -- 重複]d ["]d"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>" }  -- バッファ内の次の診断にジャンプ
+    -- 重複K ["K"] = { "<cmd>:Lspsaga hover_doc<cr>" }  -- カーソルの下にあるもののドキュメントを表示
+    -- 重複lS ["<leader>o"] = { "<cmd>:Lspsaga outline<cr>" }  -- 右側にアウトラインを表示
+    -- 重複ld ["<leader>d"] = { "<cmd>Lspsaga show_cursor_diagnostics<cr>" }  -- カーソルに関する診断を表示
+    ["<leader>ld"] = { "<cmd>Lspsaga show_line_diagnostics<cr>", desc = "● Show line diagnostics" }, -- <leader>Dから変更し、行の診断を上書き（場所も表示されるため） 行の診断を表示する
+    ["<leader>lgf"] = { "<cmd>Lspsaga lsp_finder<CR>", desc = "● Show definition and reference" }, -- gfから変更 定義とリファレンスの表示
+    ["<leader>lgD"] = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "● Get declaration" }, -- gDから変更 宣言まで取得
     ["<leader>lgd"] = {
       "<cmd>Lspsaga peek_definition<CR>",
-      desc = "ウィンドウ内で定義を見て編集を行う",
-    }, -- gdから変更
-    ["<leader>lgi"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", desc = "実装へ移動" }, -- giから変更
+      desc = "● Peek definition",
+    }, -- gdから変更 ウィンドウ内で定義を見て編集を行う
+    ["<leader>lgi"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>", desc = "● Move to implementation" }, -- giから変更 実装へ移動
 
     -- typescript
     -- TypeScript固有のキーマップ（ファイル名の変更、インポートの更新など）
     ["<leader>rf"] = {
       ":TypescriptRenameFile<CR>",
-      desc = "ファイルの名前を変更し、import を更新する",
-    },
-    ["<leader>ri"] = { ":TypescriptOrganizeImports<CR>", desc = "import を整理" },
-    ["<leader>ru"] = { ":TypescriptRemoveUnused<CR>", desc = "未使用の変数を削除" },
+      desc = "● Typescript rename file",
+    }, -- ファイルの名前を変更し、import を更新する
+    ["<leader>ri"] = { ":TypescriptOrganizeImports<CR>", desc = "● Typescript organize imports" }, -- import を整理
+    ["<leader>ru"] = { ":TypescriptRemoveUnused<CR>", desc = "● Typescript remove unused variables" }, -- 未使用の変数を削除
   },
 }
--- app/nvim/lua/astro/user/mappings.lua
+++ app/nvim/lua/astro/user/mappings.lua
@@ -5,38 +5,38 @@ return {
     -- 重複<C-w>s["<leader>sh"] = { "<C-w>s" }, -- 水平方向にウインドウを分割
     -- 重複<C-w>=["<leader>se"] = { "<C-w>=" }, -- 各ウインドウを均等の高さと幅にする
     -- 重複<leader>x ["<leader>sx"] = { "<cmd>close<CR>" }, -- ウインドウを閉じる
-    ["<Space><CR>"] = { "O<ESC>", desc = "カーソルのある行で改行する" },
-    ["PP"] = { '"0p', desc = "Space Pで最後にヤンクした文字列をペーストさせる" },
-    ["x"] = { '"_x', desc = "xで1文字削除する際にレジスタに登録しない" },
-    ["<leader>+"] = { "<C-a>", desc = "インクリメント" },
-    ["<leader>-"] = { "<C-x>", desc = "デクリメント" },
-    ["<C-w><left>"] = { "<C-w>>", desc = "ウインドウを左に広げる" },
-    ["<C-w><right>"] = { "<C-w><", desc = "ウインドウを右に広げる" },
-    ["<C-w><up>"] = { "<C-w>+", desc = "ウインドウを上に広げる" },
-    ["<C-w><down>"] = { "<C-w>-", desc = "ウインドウを下に広げる" },
-    ["<leader>to"] = { "<cmd>tabnew<CR>", desc = "新しいタブを開く" },
-    ["<leader>tx"] = { "<cmd>tabclose<CR>", desc = "現在のタブを閉じる" },
-    ["<leader>tn"] = { "<cmd>tabn<CR>", desc = "次のタブに移動する" }, -- toggle nodeに上書き
-    ["<leader>tp"] = { "<cmd>tabp<CR>", desc = "前のタブに移動する" },
-    ["˚"] = { ":m .-2<CR>==", desc = "選択した行を一行上にずらす" }, -- option + k
-    ["∆"] = { ":m .+1<CR>==", desc = "選択した行を一行下にずらす" }, -- option + j
+    ["<Space><CR>"] = { "O<ESC>", desc = "● New line" }, -- カーソルのある行で改行する
+    ["PP"] = { '"0p', desc = "● Paste the last yanked strings by Space + p" }, -- Space Pで最後にヤンクした文字列をペーストさせる
+    ["x"] = { '"_x', desc = "● Do not register one character with x deleting" }, -- xで1文字削除する際にレジスタに登録しない
+    ["<leader>+"] = { "<C-a>", desc = "● Increment" }, -- インクリメント
+    ["<leader>-"] = { "<C-x>", desc = "● Decrement" }, -- デクリメント
+    ["<C-w><left>"] = { "<C-w>>", desc = "● Expand window to the left" }, -- ウインドウを左に広げる
+    ["<C-w><right>"] = { "<C-w><", desc = "● Expand window to the right" }, -- ウインドウを右に広げる
+    ["<C-w><up>"] = { "<C-w>+", desc = "● Expand window to the up" }, -- ウインドウを上に広げる
+    ["<C-w><down>"] = { "<C-w>-", desc = "● Expand window to the down" }, -- ウインドウを下に広げる
+    ["<leader>to"] = { "<cmd>tabnew<CR>", desc = "● open new tab" }, -- 新しいタブを開く
+    ["<leader>tx"] = { "<cmd>tabclose<CR>", desc = "● Close current tab" }, -- 現在のタブを閉じる
+    ["<leader>tn"] = { "<cmd>tabn<CR>", desc = "● Move to next tab" }, -- toggle nodeに上書き、次のタブに移動する
+    ["<leader>tp"] = { "<cmd>tabp<CR>", desc = "● Move to previous tab" }, -- 前のタブに移動する
+    ["˚"] = { ":m .-2<CR>==", desc = "● Shift the selected line up" }, -- option + k 選択した行を一行上にずらす
+    ["∆"] = { ":m .+1<CR>==", desc = "● Shift the selected line down" }, -- option + j 選択した行を一行下にずらす
 
     -- bufferline
-    ["<Tab>"] = { "<Cmd>BufferLineCycleNext<CR>", desc = "タブキーで次のバッファに移動" },
-    ["<S-Tab>"] = { "<Cmd>BufferLineCyclePrev<CR>", desc = "タブキーで前のバッファに移動" },
+    ["<Tab>"] = { "<Cmd>BufferLineCycleNext<CR>", desc = "● Move to next buffer by Tab key" }, -- タブキーで次のバッファに移動
+    ["<S-Tab>"] = { "<Cmd>BufferLineCyclePrev<CR>", desc = "● Move to next buffer by Tab key" }, -- タブキーで前のバッファに移動
 
     -- multi-cursor(shift + opition + jk), vim-tmux-navigatorとの競合のためCtrl-j,kは使用できない
     [""] = {
       "<cmd>call vm#commands#add_cursor_up(0, v:count1)<cr>",
-      desc = "一行上にマルチカーソルを作成",
-    },
+      desc = "● Create multi-cursor up",
+    }, -- 一行上にマルチカーソルを作成
     ["Ô"] = {
       "<cmd>call vm#commands#add_cursor_down(0, v:count1)<cr>",
-      desc = "一行下にマルチカーソルを作成",
-    },
+      desc = "● Create multi-cursor down",
+    }, -- 一行下にマルチカーソルを作成
 
     -- vim-maximizer
-    ["<leader>sm"] = { ":MaximizerToggle<CR>", desc = "ウインドウのサイズを最大化・戻す" },
+    ["<leader>sm"] = { ":MaximizerToggle<CR>", desc = "● Maximize window" }, -- ウインドウのサイズを最大化・戻す
 
     -- telescope
     -- 重複<leader>ff ["<leader>ff] = { "<cmd>Telescope find_files<cr>") -- .gitignoreを尊重して、現在の作業ディレクトリ内のファイルを検索
@@ -49,14 +49,14 @@ return {
     -- 重複<leader>gt ["<leader>gs"] = { "<cmd>Telescope git_status<cr>" }, -- ファイルごとの現在の変更点を差分表示
     ["<leader>gfc"] = {
       "<cmd>Telescope git_bcommits<cr>",
-      desc = "現在のファイル/バッファの git コミットをリストアップ",
-    },
+      desc = "● List git commits for current file/buffer",
+    }, -- 現在のファイル/バッファの git コミットをリストアップ
 
     -- lsp
-    ["<leader>rs"] = { ":LspRestart<cr>", desc = "LSPサーバーを再起動させる" },
+    ["<leader>rs"] = { ":LspRestart<cr>", desc = "● Restart Lsp" }, -- LSPサーバーを再起動させる
 
     -- zen-mode
-    ["<leader>z"] = { ":ZenMode<cr>", desc = "Zenモード" },
+    ["<leader>z"] = { ":ZenMode<cr>", desc = "● Zen mode" }, -- Zenモード
   },
 
   i = {
@@ -65,7 +65,7 @@ return {
   },
 
   v = {
-    ["˚"] = { ":m '<-2<CR>gv-gv", desc = "選択した行を一行上にずらす" }, -- option + k
-    ["∆"] = { ":m '>+1<CR>gv-gv", desc = "選択した行を一行下にずらす" }, -- option + j
+    ["˚"] = { ":m '<-2<CR>gv-gv", desc = "● Shift the selected line up" }, -- option + k 選択した行を一行上にずらす
+    ["∆"] = { ":m '>+1<CR>gv-gv", desc = "● Shift the selected line down" }, -- option + j 選択した行を一行下にずらす
   },
 }
