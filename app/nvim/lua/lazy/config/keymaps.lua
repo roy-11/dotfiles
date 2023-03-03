3bb7f044b5a227ae26b504cd4d3efaa269eae168
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-03-03 19:13:04 +0900

    update lazy vim keymapping

--- app/nvim/lua/lazy/config/keymaps.lua
+++ app/nvim/lua/lazy/config/keymaps.lua
@@ -16,16 +16,16 @@
 -- keymap.set("Ô", "<cmd>call vm#commands#add_cursor_down(0, v:count1)<cr>") -- 一行下にマルチカーソルを作成
 
 local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
+  -- local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
   -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+  -- if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
+  -- end
 end
 
-map("n", "x", "_x", { desc = "x but not register" })
+map("n", "x", '"_x', { desc = "x but not register" })
 map("n", "PP", '"0p', { desc = "paste the last y yunk" })
 map("n", "<Space><CR>", "O<ESC>", { desc = "create new line" })
