27f8b22cc592912eae910a40fb0da74f95d52b24
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-05-08 23:13:19 +0900

    update keymaps in lazyVim

--- app/nvim/lua/lazy/config/keymaps.lua
+++ app/nvim/lua/lazy/config/keymaps.lua
@@ -2,19 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- -- zen-mode
--- keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>") -- 全ての git コミットをリストアップ
---
--- -- quickrun
--- keymap.set("n", "<leader>qr", "<cmd>QuickRun<cr>") -- ファイルを実行
---
--- -- vim-maximizer
--- keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- ウインドウのサイズを最大化・戻す
---
--- -- multi-cursor
--- keymap.set("", "<cmd>call vm#commands#add_cursor_up(0, v:count1)<cr>") -- 一行上にマルチカーソルを作成
--- keymap.set("Ô", "<cmd>call vm#commands#add_cursor_down(0, v:count1)<cr>") -- 一行下にマルチカーソルを作成
-
 local function map(mode, lhs, rhs, opts)
   -- local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
@@ -26,6 +13,16 @@ local function map(mode, lhs, rhs, opts)
   -- end
 end
 
+---------------------
+-- General Keymaps
+---------------------
+
 map("n", "x", '"_x', { desc = "x but not register" })
 map("n", "PP", '"0p', { desc = "paste the last y yunk" })
 map("n", "<Space><CR>", "O<ESC>", { desc = "create new line" })
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+map("n", "<Space>r", "<cmd>QuickRun<CR>", { desc = "run this file" })
