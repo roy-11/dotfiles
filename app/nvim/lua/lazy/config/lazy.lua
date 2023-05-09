48ac9c4e53580c6ce3588482c29639373e4a7c3a
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-05-09 17:16:24 +0900

    add copilot in lazyVim

--- app/nvim/lua/lazy/config/lazy.lua
+++ app/nvim/lua/lazy/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     -- 追記
     { import = "lazyvim.plugins.extras.linting.eslint" }, -- 追加：eslint
     { import = "lazyvim.plugins.extras.formatting.prettier" }, -- 追加：prettier
+    { import = "lazyvim.plugins.extras.coding.copilot" }, -- 追加：copilot
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
