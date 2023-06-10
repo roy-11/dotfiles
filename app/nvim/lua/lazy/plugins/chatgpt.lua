a392fabb2b4e28aa876eb9f8962be0bdd9bd9d55
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-06-10 22:44:13 +0900

    add chagpt.nvim to lazyvim

--- app/nvim/lua/lazy/plugins/chatgpt.lua
+++ app/nvim/lua/lazy/plugins/chatgpt.lua
@@ -3,7 +3,9 @@ return {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        api_key_cmd = "gpg --decrypt ~/.dotfiles/app/chatgpt.nvim/key.txt.gpg 2>>/dev/null",
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
