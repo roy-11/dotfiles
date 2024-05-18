f6d3c05eaba11dada65763700ce742f424eca038
roy-11 <63440731+roy-11@users.noreply.github.com>
2024-05-19 04:36:26 +0900

    feat: update Taskfile setting

--- app/nvim/lua/lazy/plugins/chatgpt.lua
+++ app/nvim/lua/lazy/plugins/chatgpt.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     config = function()
       require("chatgpt").setup({
-        api_key_cmd = "task keytxt",
+        api_key_cmd = "task -g keytxt",
       })
     end,
     dependencies = {
