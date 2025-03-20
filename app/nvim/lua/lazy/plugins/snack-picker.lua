59af941713a8f933678f482e8eb28533fab4a16b
roy-11 <63440731+roy-11@users.noreply.github.com>
2025-03-20 20:54:00 +0900

    chore: update nvim snacks setting

--- app/nvim/lua/lazy/plugins/snack-picker.lua
+++ app/nvim/lua/lazy/plugins/snack-picker.lua
@@ -4,7 +4,12 @@ return {
     opts.picker = opts.picker or {}
     opts.picker.formatters = opts.picker.formatters or {}
     opts.picker.formatters.file = opts.picker.formatters.file or {}
-    opts.picker.formatters.file.truncate = 200
+    opts.picker.formatters.file.truncate = 200 -- 表示されるパスを省略しない
+
+    opts.picker.win = opts.picker.win or {}
+    opts.picker.win.input = opts.picker.win.input or {}
+    opts.picker.win.input.keys = opts.picker.win.input.keys or {}
+    opts.picker.win.input.keys["<M-v>"] = "toggle_hidden" -- "<M-v>"で隠しファイルの表示/非表示をトグル
 
     -- 他の既存設定をそのまま利用
     return opts
