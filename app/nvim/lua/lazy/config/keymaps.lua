3b3acaeeac9531e041398b7476693df978220cc4
roy-11 <63440731+roy-11@users.noreply.github.com>
2025-06-01 03:06:15 +0900

    chore: update lazyvim mapping setting

--- app/nvim/lua/lazy/config/keymaps.lua
+++ app/nvim/lua/lazy/config/keymaps.lua
@@ -33,7 +33,6 @@ map("v", "ƒ", ":m '>+1<CR>gv-gv", { desc = "shift down one line in visual" }) -
 
 map("n", "<leader><Space>", LazyVim.pick("auto", { root = false }), { desc = "Find Files (cwd)" })
 map("n", "<Space>rf", "<cmd>QuickRun<CR>", { desc = "run this file" })
-map("n", "<Space>ri", '<cmd>lua require("image").setup()<CR>', { desc = "show image" })
 
 -- 検索
 map("n", "<leader>sq", "<cmd>Telescope quickfix<CR>", { desc = "quickfix" })
@@ -48,39 +47,11 @@ map(
 -- 一行上下にマルチカーソルを作成
 map("n", "ç", "<cmd>call vm#commands#add_cursor_up(0, v:count1)<CR>", { desc = "● Create multi-cursor up" })
 map("n", "√", "<cmd>call vm#commands#add_cursor_down(0, v:count1)<CR>", { desc = "● Create multi-cursor down" })
-
--- ChatGPT.nvim
-map("n", "<Space>ccc", "<cmd>ChatGPT<CR>", { desc = "ChatGPT" })
-map("n", "<Space>cca", "<cmd>ChatGPTActAs<CR>", { desc = "open prompt selection" })
-map("n", "<Space>cce", "<cmd>ChatGPTEditWithInstructions<CR>", { desc = "edit interactive window" })
-map("n", "<Space>ccg", "<cmd>ChatGPTRun grammar_correction<CR>", { desc = "grammar_correction" })
-map("n", "<Space>cct", "<cmd>ChatGPTRun translate<CR>", { desc = "translate" })
-map("n", "<Space>cck", "<cmd>ChatGPTRun keywords<CR>", { desc = "keywords" })
-map("n", "<Space>ccd", "<cmd>ChatGPTRun docstring<CR>", { desc = "docstring" })
-map("n", "<Space>cca", "<cmd>ChatGPTRun add_tests<CR>", { desc = "add_tests" })
-map("n", "<Space>cco", "<cmd>ChatGPTRun optimize_code<CR>", { desc = "optimize_code" })
-map("n", "<Space>ccs", "<cmd>ChatGPTRun summarize<CR>", { desc = "summarize" })
-map("n", "<Space>ccf", "<cmd>ChatGPTRun fix_bugs<CR>", { desc = "fix_bugs" })
-map("n", "<Space>ccx", "<cmd>ChatGPTRun explain_code<CR>", { desc = "explain_code" })
-map("n", "<Space>ccr", "<cmd>ChatGPTRun roxygen_edit<CR>", { desc = "roxygen_edit" })
-map("n", "<Space>ccl", "<cmd>ChatGPTRun code_readability_analysis<CR>", { desc = "code_readability_analysis" })
-map("v", "<Space>cce", "<cmd>ChatGPTEditWithInstructions<CR>", { desc = "edit interactive window" })
-map("v", "<Space>ccg", "<cmd>ChatGPTRun grammar_correction<CR>", { desc = "grammar_correction" })
-map("v", "<Space>cct", "<cmd>ChatGPTRun translate<CR>", { desc = "translate" })
-map("v", "<Space>cck", "<cmd>ChatGPTRun keywords<CR>", { desc = "keywords" })
-map("v", "<Space>ccd", "<cmd>ChatGPTRun docstring<CR>", { desc = "docstring" })
-map("v", "<Space>cca", "<cmd>ChatGPTRun add_tests<CR>", { desc = "add_tests" })
-map("v", "<Space>cco", "<cmd>ChatGPTRun optimize_code<CR>", { desc = "optimize_code" })
-map("v", "<Space>ccs", "<cmd>ChatGPTRun summarize<CR>", { desc = "summarize" })
-map("v", "<Space>ccf", "<cmd>ChatGPTRun fix_bugs<CR>", { desc = "fix_bugs" })
-map("v", "<Space>ccx", "<cmd>ChatGPTRun explain_code<CR>", { desc = "explain_code" })
-map("v", "<Space>ccr", "<cmd>ChatGPTRun roxygen_edit<CR>", { desc = "roxygen_edit" })
-map("v", "<Space>ccl", "<cmd>ChatGPTRun code_readability_analysis<CR>", { desc = "code_readability_analysis" })
-
 -- Image
-map(
-  "n",
-  "<Space>fi",
-  "<cmd>lua require('telescope').extensions.media_files.media_files()<CR>",
-  { desc = "preview images" }
-)
+-- map("n", "<Space>ri", '<cmd>lua require("image").setup()<CR>', { desc = "show image" })
+-- map(
+--   "n",
+--   "<Space>fi",
+--   "<cmd>lua require('telescope').extensions.media_files.media_files()<CR>",
+--   { desc = "preview images" }
+-- )
