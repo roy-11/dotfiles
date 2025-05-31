60e72aecd6c95038a20e813a10d5ea94ea0fcb9a
roy-11 <63440731+roy-11@users.noreply.github.com>
2025-06-01 02:59:18 +0900

    chore: update lazyvim autocmds setting

--- app/nvim/lua/lazy/config/autocmds.lua
+++ app/nvim/lua/lazy/config/autocmds.lua
@@ -12,7 +12,9 @@ end
 vim.api.nvim_create_autocmd("InsertLeave", {
   desc = "ノーマルモードに戻るとき英数入力にする",
   pattern = "*",
-  command = ":silent !/opt/homebrew/bin/im-select com.apple.keylayout.ABC",
+  callback = function()
+    vim.fn.jobstart({ "/opt/homebrew/bin/im-select", "com.apple.keylayout.ABC" }, { detach = true })
+  end,
 })
 
 -- Hurl ファイルを開いたときにファイルタイプを自動設定
